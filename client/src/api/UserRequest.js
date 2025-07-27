8357b625c8f8a9e7dfadad4a880140e76954c927
aryanwebd35 <127021841+aryanwebd35@users.noreply.github.com>
2025-07-28 01:32:10 +0530

    Update UserRequest.js

--- client/src/api/UserRequest.js
+++ client/src/api/UserRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({baseURL:'http://localhost:3000'})
+const API = axios.create({baseURL:'https://psychocorp-client-1.onrender.com'})
 export const getUser = (userId) => API.get(`/user/${userId}`);
 
 // export default userChats
