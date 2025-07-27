38f6f597b4d4a3553fa6ca64abbffe28d7bb2261
aryanwebd35 <127021841+aryanwebd35@users.noreply.github.com>
2025-07-28 01:31:57 +0530

    Update MessageRequest.js

--- client/src/api/MessageRequest.js
+++ client/src/api/MessageRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 
-const API = axios.create({baseURL:'http://localhost:3000'})
+const API = axios.create({baseURL:'https://psychocorp-client-1.onrender.com'})
 export const getMessages = (id) => API.get(`/message/${id}`)
 export const addMessage = (data) => API.post('/message/',data)
