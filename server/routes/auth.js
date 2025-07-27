d6896b8a7b8d87e7b73e3a2820531456378cefce
aryanwebd35 <127021841+aryanwebd35@users.noreply.github.com>
2025-07-28 01:34:14 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -116,7 +116,7 @@ router.post('/reset-password',(req,res)=>{
                     subject:"password reset",
                     html:`
                     <p>You requested for password reset</p>
-                    <h5>click in this <a href="http://localhost:3000/reset/${token}">link</a> to reset password</h5>
+                    <h5>click in this <a href="https://psychocorp-client-1.onrender.com/reset/${token}">link</a> to reset password</h5>
                     `
                 })
                 res.json({message:"check your email"})
@@ -243,7 +243,7 @@ router.post('/docreset-password',(req,res)=>{
                     subject:"password reset",
                     html:`
                     <p>You requested for password reset</p>
-                    <h5>click in this <a href="http://localhost:3000/docreset/${token}">link</a> to reset password</h5>
+                    <h5>click in this <a href="https://psychocorp-client-1.onrender.com/docreset/${token}">link</a> to reset password</h5>
                     `
                 })
                 res.json({message:"check your email"})
